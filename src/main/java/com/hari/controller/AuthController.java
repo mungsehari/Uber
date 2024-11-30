2cff91f440a8b56adf838bb0b273650f5a46bdb7
mungsehari <mungse.hari@gmail.com>
2024-11-30 10:35:34 +0530

    Auth Controller create

--- src/main/java/com/hari/controller/AuthController.java
+++ src/main/java/com/hari/controller/AuthController.java
@@ -1,22 +1,33 @@
 package com.hari.controller;
 
+import com.hari.service.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hari.model.Passenger;
+import com.hari.dto.PassengerDto;
 import com.hari.request.PassengerSignUpRequestDto;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    @PostMapping("/singup/passenger")
-    public ResponseEntity<Passenger> singUp(@RequestBody PassengerSignUpRequestDto requestDto) {
-        // Implement logic for signing up a passenger
-        return null;
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+
+
+    @PostMapping("/signup/passenger")
+    public ResponseEntity<PassengerDto> singUp(@RequestBody PassengerSignUpRequestDto requestDto) {
+      PassengerDto response=  authService.signupPassenger(requestDto);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
 }
