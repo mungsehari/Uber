cc5b5bb4da9b26b483523a96cc8897e14a718973
mungsehari <mungse.hari@gmail.com>
2024-11-30 10:59:01 +0530

    PassengerDto dto create

---
+++ src/main/java/com/hari/dto/PassengerDto.java
@@ -0,0 +1,37 @@
+package com.hari.dto;
+
+import java.util.Date;
+
+import com.hari.model.Passenger;
+
+import lombok.*;
+
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PassengerDto {
+
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String phoneName;
+    private Date createAt;
+
+
+    public static PassengerDto from(Passenger p) {
+
+        PassengerDto result = PassengerDto.builder()
+                .id(p.getId().toString())
+                .name(p.getName())
+                .createAt(p.getCreatedAt())
+                .email(p.getEmail())
+                .password(p.getPassword())
+                .phoneName(p.getPhoneName())
+                .build();
+        return result;
+    }
+}
