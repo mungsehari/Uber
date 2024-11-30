57b7dde6fb4763ebb9fe30a083a26872a089090d
mungsehari <mungse.hari@gmail.com>
2024-11-30 10:38:51 +0530

    booking entity create

---
+++ src/main/java/com/hari/model/Booking.java
@@ -0,0 +1,45 @@
+package com.hari.model;
+
+import java.util.Date;
+
+import com.hari.status.BookingStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking extends BaseModel {
+
+    @Enumerated(value = EnumType.STRING)
+    private BookingStatus bookingStatus;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date startTime;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date endTime;
+
+    private Long totalDistance;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Driver driver;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Passenger passenger;
+
+}
