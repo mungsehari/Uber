c4d5c401bcdc91e9f118c02d2d07cb4025dcc43a
mungsehari <mungse.hari@gmail.com>
2024-12-02 18:16:51 +0530

    JwtConstant, JwtProvider and JwtTokenValidator create

---
+++ src/main/java/com/hari/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.hari.config;
+
+public class JwtConstant {
+
+
+    public static final String SECRET_KEY = "ankjpiirqwrjjknvnvlakdfapfjdjakkcpiuwrqjfkcmkfkfbljssdgdslgdsjaag";
+    public static final String JWT_HEADER = "Authorization";
+}
