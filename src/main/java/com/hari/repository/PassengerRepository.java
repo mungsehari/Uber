1fac570f90e48faf6f54a622a8c6288e5a004b1d
mungsehari <mungse.hari@gmail.com>
2024-12-02 18:43:45 +0530

    create a findPassengerByEmail

--- src/main/java/com/hari/repository/PassengerRepository.java
+++ src/main/java/com/hari/repository/PassengerRepository.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import com.hari.model.Passenger;
 
+import java.util.Optional;
+
 @Repository
 public interface PassengerRepository extends JpaRepository<Passenger, Long> {
+    Optional<Passenger> findPassengerByEmail(String email);
 
 }
