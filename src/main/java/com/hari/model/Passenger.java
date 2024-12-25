8cdff729d639f9ca342d7592e7018ef83ea6b734
mungsehari <mungse.hari@gmail.com>
2024-12-25 10:15:14 +0530

    add : entity

--- src/main/java/com/hari/model/Passenger.java
+++ src/main/java/com/hari/model/Passenger.java
@@ -9,6 +9,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,15 +29,28 @@ public class Passenger extends BaseModel {
     @Column(nullable = false)
     private String name;
 
-   @Column(nullable = false)
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
-    private String phoneName;
-
     @OneToMany(mappedBy = "passenger")
-    private List<Booking> bookingList = new ArrayList<Booking>();
+    private List<Booking> bookings = new ArrayList<>();
+
+    @OneToOne
+    private Booking activeBooking;
+
+    @DecimalMin(value = "0.00", message = "Rating must be grater than or equal to 0.00")
+    @DecimalMax(value = "5.00", message = "Rating must be less than or equal to 5.00")
+    private Double rating;
+
+    @OneToOne
+    private ExactLocation lastKnownLocation;
+
+    @OneToOne
+    private ExactLocation home;
 }
