c984b2f8b7aad06253f4107d784a89ec7e5e161d
mungsehari <mungse.hari@gmail.com>
2024-12-25 10:13:16 +0530

    create a DBConstant entity

---
+++ src/main/java/com/hari/model/DBConstant.java
@@ -0,0 +1,18 @@
+package com.hari.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DBConstant extends BaseModel {
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    private String value;
+}
