e81bda52a9bbc17431539f556adcabdd2adc74da
mungsehari <mungse.hari@gmail.com>
2024-12-25 10:15:09 +0530

    add : entity

--- src/main/java/com/hari/model/Driver.java
+++ src/main/java/com/hari/model/Driver.java
@@ -2,14 +2,15 @@ package com.hari.model;
 
 import java.util.List;
 
+import com.hari.status.DriverApprovalStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -32,6 +33,30 @@ public class Driver extends BaseModel {
 
     private String phoneNumber;
 
+    private String aadharCard;
+
+    @OneToOne(mappedBy = "driver", cascade = CascadeType.ALL.ALL)
+    private Car car;
+
+
+    @Enumerated(value = EnumType.STRING)
+    private DriverApprovalStatus driverApprovalStatus;
+
+    @OneToOne
+    private ExactLocation lastKnownLocation;
+
+    @OneToOne
+    private ExactLocation home;
+
+    private String activeCity;
+
+    @DecimalMin(value = "0.00", message = "Rating must be grater than or equal to 0.00")
+    @DecimalMax(value = "5.00", message = "Rating must be less than or equal to 5.00")
+    private Double rating;
+
+    private boolean isAvailable;
+
+    // 1 : n , Driver : Booking
     @OneToMany(mappedBy = "driver")
     @Fetch(FetchMode.SUBSELECT)
     private List<Booking> bookings;
