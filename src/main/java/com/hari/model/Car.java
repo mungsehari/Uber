dad8af320df43a6b656319ccac9d5027104732a4
mungsehari <mungse.hari@gmail.com>
2024-12-25 10:12:27 +0530

    create a Car entity

---
+++ src/main/java/com/hari/model/Car.java
@@ -0,0 +1,28 @@
+package com.hari.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car extends BaseModel {
+    @Column(unique = true, nullable = false)
+    private String plateNumber;
+
+    @ManyToOne
+    private Color color;
+
+    private String brand;
+
+    private String model;
+
+    @Enumerated(value = EnumType.STRING)
+    private CarType carType;
+
+    @OneToOne
+    private Driver driver;
+}
