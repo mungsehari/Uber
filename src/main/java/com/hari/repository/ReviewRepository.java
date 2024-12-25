26d4add43c042744e7954bce590eee6372d136cd
mungsehari <mungse.hari@gmail.com>
2024-12-25 15:04:25 +0530

    create : ReviewController and ReviewDto and ReviewRepository and ReviewService and ReviewServiceImpl

---
+++ src/main/java/com/hari/repository/ReviewRepository.java
@@ -0,0 +1,22 @@
+package com.hari.repository;
+
+import com.hari.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Integer countAllByRatingIsLessThanEqual(Integer givenRating);
+
+    List<Review> findAllByRatingIsLessThanEqual(Integer givenRating);
+
+    List<Review> findAllByCreatedAtBefore(Date date);
+
+    @Query("select r from Booking b inner join Review r where b.id = :bookingId")
+    Review findReviewByBookingId(Long bookingId);
+}
---
+++ src/main/java/com/hari/service/ReviewServiceImpl.java
@@ -0,0 +1,70 @@
+package com.hari.service;
+
+import com.hari.model.Review;
+import com.hari.repository.ReviewRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.hibernate.FetchNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+    private final ReviewRepository reviewRepository;
+
+    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    @Override
+    public Optional<Review> findReviewById(Long id) {
+        Optional<Review> review;
+        try {
+            review = this.reviewRepository.findById(id);
+            if (review.isEmpty()) {
+                throw new EntityNotFoundException("Review with id " + id + " not found");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            if(e.getClass() == EntityNotFoundException.class){
+                throw new FetchNotFoundException("Review with id " + id + " not found", id);
+            }
+            throw new FetchNotFoundException("Unable to fetch, PLease try again later!", id);
+        }
+        return review;
+    }
+
+    @Override
+    public List<Review> findAllReviews() {
+        return this.reviewRepository.findAll();
+    }
+
+    @Override
+    public boolean deleteReviewById(Long id) {
+        try {
+            Review review = this.reviewRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+            this.reviewRepository.delete(review);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public Review publishReview(Review review) {
+        return this.reviewRepository.save(review);
+    }
+
+    @Override
+    public Review updateReview(Long id, Review newReviewData) {
+        Review review = this.reviewRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        if(newReviewData.getRating() != null){
+            review.setRating(newReviewData.getRating());
+        }
+        if(newReviewData.getContent() != null){
+            review.setContent(newReviewData.getContent());
+        }
+        return this.reviewRepository.save(review);
+    }
+}
