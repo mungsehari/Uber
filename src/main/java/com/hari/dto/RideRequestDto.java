205205796024ca04f7a0aab5de6298d51be37255
mungsehari <mungse.hari@gmail.com>
2024-12-25 15:04:38 +0530

    create : RideRequestDto

---
+++ src/main/java/com/hari/dto/RideRequestDto.java
@@ -0,0 +1,24 @@
+package com.hari.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RideRequestDto {
+
+    private Long passengerId;
+
+//    private ExactLocation startLocation;
+//
+//    private ExactLocation endLocation;
+
+    private List<Long> driverIds;
+
+    private Long bookingId;
+}
